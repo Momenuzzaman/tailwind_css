6cf7eec6089dd1edf3f00b84bce7c1e5fad45355
mdemon420 <mdeamon4@gmail.com>
2022-06-30 12:42:25 +0600

    responsive

--- src/Components/MrBean/MrBean.js
+++ src/Components/MrBean/MrBean.js
@@ -3,11 +3,13 @@ import img from '../../img/download.jpg'
 
 const MrBean = () => {
     return (
-        <div className="h-screen flex items-center justify-center bg-gray-200">
-            <div className="max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-3">
-                <img className="h-21 max-auto rounded-full ring-4 ring-green-300" src={img} alt="/" />
-                <div className="space-y-4">
-                    <div className="text-center space-y-0.5">
+        <div className="h-screen flex items-center justify-center bg-gray-200 ">
+            <div className="max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-3 sm:flex sm:items-center
+            sm:py-4 sm:space-y-0
+            sm:space-x-6">
+                <img className="sm:mx-0 h-24  max-auto rounded-full ring-4 ring-green-300 sm:flex-shrink-0" src={img} alt="/" />
+                <div className="space-y-4 text-center sm:text-left">
+                    <div className="space-y-0.5">
                         <p className="text-lg text-black font-semibold" >I am learning Code</p>
                         <p className="text-gray-500 font-medium">Prayer for me</p>
                     </div>
